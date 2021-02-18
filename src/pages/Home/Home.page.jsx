a1acbd181d6e12667753401f26623d2a5f772f6f
Gonzalo Campos <54908614+Gonzz-96@users.noreply.github.com>
2021-02-17 23:47:56 -0600

    Playing with grid.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../../components/Header'
 
+const Title = styled.p`
+  font-size: 2rem;
+  text-align: center;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-row-gap: 10px;
+  justify-content: space-around;
+`;
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: ${props => props.color || 'green'};
+`;
+
 const HomePage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Title>Welcome to my minichallenge :)</Title>
+      <GridContainer>
+        <Box color="red"></Box>
+        <Box color="blue"></Box>
+        <Box></Box>
+        <Box color="red"></Box>
+        
+        <Box color="red"></Box>
+        <Box color="blue"></Box>
+        <Box></Box>
+
+      </GridContainer>
+    </>
+  );
 }
 
 export default HomePage;
