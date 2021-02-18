33191d365137db952698f79bb9e1a04105a191f2
Gonzalo Campos <54908614+Gonzz-96@users.noreply.github.com>
2021-02-17 23:28:44 -0600

    Create base Header.

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HeaderContainer } from './styled';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <p>Menu and Searchbar</p>
+      <p>Theme and Login</p>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  height: 65px;
+  width: 100%;
+  padding: 0 20px 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #1C5476;
+`;
+
+export { HeaderContainer };
