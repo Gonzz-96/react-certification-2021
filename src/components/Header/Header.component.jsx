51904349b54b0c9e1fca794ed47611ae2dce7c65
Gonzalo Campos <erick.campos@wizeline.com>
2021-02-18 22:38:14 -0600

    Finish Header component.

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { HeaderContainer } from './styled';
+import {
+  HeaderContainer,
+  LeftContent,
+  RightContent,
+  MenuIcon,
+  SearchBar,
+  UserIcon,
+} from './styled';
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <p>Menu and Searchbar</p>
-      <p>Theme and Login</p>
+      <LeftContent>
+        <MenuIcon src={`${process.env.PUBLIC_URL}/menu_icon.png`} />
+        <SearchBar />
+      </LeftContent>
+      <RightContent>
+        <p style={{ color: 'white' }}>Dark Mode</p>
+        <UserIcon src={`${process.env.PUBLIC_URL}/user_icon.png`} />
+      </RightContent>
     </HeaderContainer>
   );
 };
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
-  height: 65px;
+  height: 7%;
   width: 100%;
   padding: 0 20px 0 20px;
   display: flex;
@@ -10,4 +10,56 @@ const HeaderContainer = styled.header`
   background-color: #1c5476;
 `;
 
-export { HeaderContainer };
+const LeftContent = styled.div`
+  height: 50%;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+  }
+`;
+
+const RightContent = styled.div`
+  height: 50%;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+
+  @media screen and (max-width: 900px) {
+    width: 0%;
+    display: none;
+  }
+`;
+
+const MenuIcon = styled.img`
+  border-radius: 30px;
+  cursor: pointer;
+`;
+
+const SearchBar = styled.input`
+  width: 240px;
+  margin-left: 50px;
+  border: none;
+  border-radius: 30px;
+  padding: 0 5px 0 5px;
+
+  &:focus {
+    outline: none;
+  }
+
+  @media screen and (max-width: 900px) {
+    width: 800px;
+  }
+`;
+
+const UserIcon = styled(MenuIcon)`
+  height: 50px;
+  margin-left: 20px;
+  background-color: white;
+`;
+
+export { HeaderContainer, LeftContent, RightContent, MenuIcon, UserIcon, SearchBar };
