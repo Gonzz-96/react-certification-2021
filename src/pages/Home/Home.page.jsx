e969c50c47cecfe7d6ef6a906f8a958646319779
Gonzalo Campos <erick.campos@wizeline.com>
2021-02-19 11:25:28 -0600

    Extract logic to independent function for testing.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -5,26 +5,30 @@ import VideoCard from '../../components/VideoCard';
 import { GridContainer, Title } from './styled';
 import { items } from '../../mock/youtube-videos-mock.json';
 
+const getVideoCardsFromList = (list) => {
+  return list.map((i) => {
+    const { snippet } = i;
+    const { title, description, thumbnails } = snippet;
+    return (
+      <VideoCard
+        title={title}
+        description={description}
+        thumbnail={thumbnails.high.url}
+      />
+    );
+  });
+};
+
 const HomePage = () => {
   return (
     <>
       <Header />
       <Title>Welcome to my minichallenge :)</Title>
-      <GridContainer>
-        {items.map((i) => {
-          const { snippet } = i;
-          const { title, description, thumbnails } = snippet;
-          return (
-            <VideoCard
-              title={title}
-              description={description}
-              thumbnail={thumbnails.high.url}
-            />
-          );
-        })}
-      </GridContainer>
+      <GridContainer>{getVideoCardsFromList(items)}</GridContainer>
     </>
   );
 };
 
 export default HomePage;
+
+export { getVideoCardsFromList };
