b38df15b8b3a6eb024f6f17a3331448b72a296e3
Gonzalo Campos <erick.campos@wizeline.com>
2021-02-19 11:39:22 -0600

    Move Home.spec.jsx test to pages folder.

--- src/pages/_test/Home.spec.jsx
+++ src/pages/_test/Home.spec.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { getVideoCardsFromList } from '../../pages/Home/Home.page';
+import { getVideoCardsFromList } from '../Home/Home.page';
 
-import { items } from './../../mock/youtube-videos-mock.json';
+import { items } from '../../mock/youtube-videos-mock.json';
 
 describe('Test Home page logic', () => {
   it('Check number of items is the same as number of components', () => {
