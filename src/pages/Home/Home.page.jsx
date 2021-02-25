d719f763d02b3df21b651b22db483829defb8473
Gonzalo Campos <erick.campos@wizeline.com>
2021-02-25 14:05:48 -0600

    Fix PR review comments.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -5,30 +5,25 @@ import VideoCard from '../../components/VideoCard';
 import { GridContainer, Title } from './styled';
 import { items } from '../../mock/youtube-videos-mock.json';
 
-const getVideoCardsFromList = (list) => {
-  return list.map((i) => {
-    const { snippet } = i;
-    const { title, description, thumbnails } = snippet;
-    return (
-      <VideoCard
-        title={title}
-        description={description}
-        thumbnail={thumbnails.high.url}
-      />
-    );
-  });
-};
-
 const HomePage = () => {
   return (
-    <>
+    <React.Fragment>
       <Header />
       <Title>Welcome to my minichallenge :)</Title>
-      <GridContainer>{getVideoCardsFromList(items)}</GridContainer>
-    </>
+      <GridContainer>
+        {items.map((i) => {
+          const { title, description, thumbnails } = i.snippet;
+          return (
+            <VideoCard
+              title={title}
+              description={description}
+              thumbnail={thumbnails.high.url}
+            />
+          );
+        })}
+      </GridContainer>
+    </React.Fragment>
   );
 };
-
-export { getVideoCardsFromList };
 
 export default HomePage;
