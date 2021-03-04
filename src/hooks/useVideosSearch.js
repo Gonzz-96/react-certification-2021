2077dc432908e9d608de39fed5c8044b74648ab7
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-04 17:09:54 -0600

    Remove useless logs.

--- src/hooks/useVideosSearch.js
+++ src/hooks/useVideosSearch.js
@@ -4,12 +4,10 @@ const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/';
 
 const useVideosSearch = (keyword = 'wizeline') => {
   if (process.env.NODE_ENV === 'development') {
-    console.log('Local videos');
     return new Promise((resolve, reject) => {
       resolve(items);
     });
   } else if (process.env.NODE_ENV === 'production') {
-    console.log('Remote videos');
     return fetch(
       `${YOUTUBE_API_BASE_URL}search?q=${keyword}&maxResults=25&part=snippet&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
     )
