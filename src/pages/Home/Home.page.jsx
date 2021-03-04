e7402883fadae6bddff9e002133e8e5982f40538
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-04 15:59:05 -0600

    Use hook instead of json file.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import VideoCard from '../../components/VideoCard';
 
 import { GridContainer, Title } from './styled';
-import { items } from '../../mock/youtube-videos-mock.json';
+
+import { useVideos } from '../../client/useVideos';
 
 const HomePage = () => {
+  const videos = useVideos();
+  const [videoCards, setVideoCards] = useState([]);
+
+  useEffect(() => {
+    videos.then((result) => {
+      const newVideoCards = result.map((i) => {
+        const { title, description, thumbnails } = i.snippet;
+        return (
+          <VideoCard
+            title={title}
+            description={description}
+            thumbnail={thumbnails.high.url}
+          />
+        );
+      });
+      setVideoCards(newVideoCards);
+    });
+  }, []);
+
   return (
     <React.Fragment>
       <Header />
       <Title>Welcome to my minichallenge :)</Title>
-      <GridContainer>
-        {items.map((i) => {
-          const { title, description, thumbnails } = i.snippet;
-          return (
-            <VideoCard
-              title={title}
-              description={description}
-              thumbnail={thumbnails.high.url}
-            />
-          );
-        })}
-      </GridContainer>
+      <GridContainer>{videoCards}</GridContainer>
     </React.Fragment>
   );
 };
