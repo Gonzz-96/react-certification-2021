cded7f604d001532a52a10947aec5ea2d8153938
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-04 15:58:55 -0600

    Create hook to load videos either locally or remotely.

---
+++ src/client/useVideos.js
@@ -0,0 +1,19 @@
+import { items } from '../mock/youtube-videos-mock.json';
+
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/';
+
+const useVideos = (keyword = 'wizeline') => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Local videos');
+    return new Promise((resolve, reject) => {
+      resolve(items);
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    console.log('Remote videos');
+    return fetch(
+      `${YOUTUBE_API_BASE_URL}search?q=${keyword}&maxResults=25&part=snippet&key=AIzaSyCrDMiVWZcjDW-ZEJnxyoOVY860LhLZ2Hg`
+    ).then;
+  }
+};
+
+export { useVideos };
