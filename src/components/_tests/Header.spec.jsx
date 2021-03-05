fc741e8e53e76e53250abf56e62486d9c9971c00
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-05 14:08:01 -0600

    Write more tests for Header component.

--- src/components/_tests/Header.spec.jsx
+++ src/components/_tests/Header.spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Header from '../Header';
 
 describe('Tests on VideoCard component', () => {
@@ -22,5 +22,33 @@ describe('Tests on VideoCard component', () => {
     const { getByText } = render(<Header />);
 
     expect(getByText(text).tagName).toBe('P');
+  });
+
+  it('Check on submit working correctly', () => {
+    let initialValue = 0;
+    let counter = initialValue;
+    const onSubmit = () => {
+      counter++;
+    };
+
+    const { container } = render(<Header onSubmit={onSubmit} />);
+    fireEvent.submit(container.querySelector('form'));
+
+    expect(counter).toBe(initialValue + 1);
+  });
+
+  it('Check onChange working correctly', () => {
+    const value = 'Hello, world';
+
+    const onSubmit = (text) => {
+      expect(text).toBe(value);
+    };
+
+    const { container } = render(<Header onSubmit={onSubmit} />);
+    fireEvent.change(container.querySelector('input'), {
+      target: { value: 'Hello, world' },
+    });
+
+    fireEvent.submit(container.querySelector('form'));
   });
 });
