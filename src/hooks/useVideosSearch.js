ddd7015ccd5b9a75fb623ce52441d27744f841a3
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-05 16:43:06 -0600

    Refactor useVideosSearch.

--- src/hooks/useVideosSearch.js
+++ src/hooks/useVideosSearch.js
@@ -1,26 +1,29 @@
+import { useEffect, useState } from 'react';
 import { items } from '../mock/youtube-videos-mock.json';
 
 const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/';
 
 const useVideosSearch = (keyword = 'wizeline') => {
-  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-    return new Promise((resolve, reject) => {
-      resolve(items);
-    });
-  } else if (process.env.NODE_ENV === 'production') {
-    return fetch(
-      `${YOUTUBE_API_BASE_URL}search?q=${keyword}&maxResults=25&part=snippet&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then(
-        (data) =>
-          new Promise((resolve, reject) => {
-            resolve(data.items);
-          })
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const response = await fetch(
+        `${YOUTUBE_API_BASE_URL}search?q=${keyword}&maxResults=25&part=snippet&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
       );
-  }
+      const { items } = await response.json();
+
+      setVideos(items);
+    };
+
+    if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+      setVideos(items);
+    } else {
+      fetchVideos();
+    }
+  }, [keyword]);
+
+  return videos;
 };
 
 export { useVideosSearch };
