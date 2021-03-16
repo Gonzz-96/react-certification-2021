e3d44947f6d1f5641cd1db20f884e33e4fe84dcb
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-15 20:47:11 -0600

    Use ThemeContext in Home compoonent.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,36 +1,46 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Header from '../../components/Header';
 import VideoCard from '../../components/VideoCard';
 import VideoDetail from '../VideoDetail';
 import { GridContainer, Title } from './styled';
 
 import { useVideosSearch } from '../../hooks/useVideosSearch';
+import ThemeContext from '../../context/ThemeContext';
 
 const HomePage = () => {
   const [selectedVideo, setSelectedVideo] = useState();
   const videos = useVideosSearch();
+  const { theme, dispatch } = useContext(ThemeContext);
+
+  const { backgroundColor, color } = theme;
+
+  setTimeout(() => {
+    dispatch({ type: 'dark' });
+  }, 5000);
 
   let content;
   if (!selectedVideo) {
     content = (
       <React.Fragment>
-        <Header />
-        <Title>Welcome to my minichallenge :)</Title>
-        <GridContainer>
-          {videos.map((i) => {
-            const { title, description, thumbnails } = i.snippet;
-            return (
-              <VideoCard
-                title={title}
-                onClick={() => {
-                  setSelectedVideo(i);
-                }}
-                description={description}
-                thumbnail={thumbnails.high.url}
-              />
-            );
-          })}
-        </GridContainer>
+        <div style={{ backgroundColor }}>
+          <Header />
+          <Title theme={{ color }}>Welcome to my minichallenge :)</Title>
+          <GridContainer theme={{ color }}>
+            {videos.map((i) => {
+              const { title, description, thumbnails } = i.snippet;
+              return (
+                <VideoCard
+                  title={title}
+                  onClick={() => {
+                    setSelectedVideo(i);
+                  }}
+                  description={description}
+                  thumbnail={thumbnails.high.url}
+                />
+              );
+            })}
+          </GridContainer>
+        </div>
       </React.Fragment>
     );
   } else {
