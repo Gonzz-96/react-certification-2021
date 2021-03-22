f5ac1a1d5a13d17c8e6dff262f342b5787cfa2d2
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-22 16:37:04 -0600

    Remove conditional rendering in Home page.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,52 +1,37 @@
 import React, { useContext, useState } from 'react';
 
 import VideoCard from '../../components/VideoCard';
-import VideoDetail from '../VideoDetail';
 import { GridContainer, Title } from './styled';
 
 import { useVideosSearch } from '../../hooks/useVideosSearch';
 import ThemeContext from '../../context/ThemeContext';
 
 const HomePage = () => {
-  const [selectedVideo, setSelectedVideo] = useState();
   const videos = useVideosSearch();
   const { theme } = useContext(ThemeContext);
 
   const { backgroundColor, color } = theme;
 
-  let content;
-  if (!selectedVideo) {
-    content = (
-      <React.Fragment>
-        <div style={{ backgroundColor }}>
-          <Title theme={{ color }}>Welcome to my minichallenge :)</Title>
-          <GridContainer theme={{ color }}>
-            {videos.map((i) => {
-              const { title, description, thumbnails } = i.snippet;
-              return (
-                <VideoCard
-                  title={title}
-                  onClick={() => {
-                    setSelectedVideo(i);
-                  }}
-                  description={description}
-                  thumbnail={thumbnails.high.url}
-                />
-              );
-            })}
-          </GridContainer>
-        </div>
-      </React.Fragment>
-    );
-  } else {
-    content = (
-      <React.Fragment>
-        <VideoDetail video={selectedVideo} restOfVideos={videos} />
-      </React.Fragment>
-    );
-  }
-
-  return content;
+  return (
+    <React.Fragment>
+      <div style={{ backgroundColor }}>
+        <Title theme={{ color }}>Welcome to my minichallenge :)</Title>
+        <GridContainer theme={{ color }}>
+          {videos.map((i) => {
+            const { title, description, thumbnails } = i.snippet;
+            return (
+              <VideoCard
+                id={i.id.videoId}
+                title={title}
+                description={description}
+                thumbnail={thumbnails.high.url}
+              />
+            );
+          })}
+        </GridContainer>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
