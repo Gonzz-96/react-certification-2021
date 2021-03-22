b0f8b896ac700672707bded294805b59d3f863e2
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-22 17:50:00 -0600

    Update videos context in Home page.

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import VideoCard from '../../components/VideoCard';
 import { GridContainer, Title } from './styled';
 
 import { useVideosSearch } from '../../hooks/useVideosSearch';
 import ThemeContext from '../../context/ThemeContext';
+import SearchVideosContext from '../../context/SearchVideosContext';
 
 const HomePage = () => {
   const videos = useVideosSearch();
   const { theme } = useContext(ThemeContext);
-
   const { backgroundColor, color } = theme;
+  const { dispatch: searchVideosDispatch } = useContext(SearchVideosContext);
+
+  useEffect(() => {
+    searchVideosDispatch({ type: 'NEW_VIDEOS', payload: videos });
+  }, [videos]);
 
   return (
     <React.Fragment>
