3acce12c3dca4bdc3ac5b789856c491606dbc97a
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-22 15:59:45 -0600

    Remove superfluous comopnents.

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -3,11 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
-import LoginPage from '../../pages/Login';
 import NotFound from '../../pages/NotFound';
-import SecretPage from '../../pages/Secret';
-import Private from '../Private';
-import Fortune from '../Fortune';
 import Layout from '../Layout';
 import { random } from '../../utils/fns';
 import KeywordContext, { reducer as keywordReducer } from '../../context/SearchContext';
@@ -49,18 +45,11 @@ function App() {
                   <HomePage />
                 </KeywordContext.Provider>
               </Route>
-              <Route exact path="/login">
-                <LoginPage />
-              </Route>
-              <Private exact path="/secret">
-                <SecretPage />
-              </Private>
               <Route path="*">
                 <NotFound />
               </Route>
             </Switch>
           </ThemeContext.Provider>
-          <Fortune />
         </Layout>
       </AuthProvider>
     </BrowserRouter>
