829c2e8d621f3af0c0eba9ab1493103b4350625e
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-24 14:05:49 -0600

    Get favorites from storage.

--- src/pages/Favorites/Favorites.page.jsx
+++ src/pages/Favorites/Favorites.page.jsx
@@ -1,18 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import VideoCard from '../../components/VideoCard';
 import { Title, GridContainer } from './styled';
 import ThemeContext from '../../context/ThemeContext';
+import { storage } from '../../utils/storage';
 
 const Favorites = ({ videos = [] }) => {
   const { theme } = useContext(ThemeContext);
   const { backgroundColor, color } = theme;
+
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    setFavorites(storage.get('favorites') || []);
+  }, []);
 
   return (
     <React.Fragment>
       <div style={{ backgroundColor }}>
         <Title theme={{ color }}>Favorites</Title>
         <GridContainer theme={{ color }}>
-          {videos.map((i) => {
+          {favorites.map((i) => {
             return <VideoCard video={i} />;
           })}
         </GridContainer>
